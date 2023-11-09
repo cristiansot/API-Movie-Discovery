7fe1e0fdeda70047b546fa3ede2436161c60a0b4
cristiansot <soluciongrafix@gmail.com>
2023-11-09 14:04:54 -0300

    website working, method get calling two variables done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,40 @@
+const API_KEY = `96a061b7c4dfa7a6d5bcdc9f1733e583`;
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`; // Movie API
 
-const API_KEY = `96a061b7c4dfa7a6d5bcdc9f1733e583`
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}` //Movie API
+const loadMovies = async () => { //https://www.youtube.com/watch?v=GtIdmfBSoHw&list=PLpYu7Wkyi5ruXL-HBsw61NmqZy2yLEqXc&index=62&ab_channel=CodingInColour
+  
+    try {
+    const response = await fetch(API_URL);
 
-const loadMovies = async() => {
+    let movies = '';
+    // let moviesPoster = '';
+    // let moviesVote = '';
+    const data = await response.json();
+    data.results.forEach(movie => { //goes through the array looking for the titles and posters
 
-	try {
-		const response = await fetch(API_URL);
-		console.log(response);
+        // movies += `<h2>${movie.title}</h2><img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"></img>`;
+        // movies += `<h2>${movie.title}<img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"></img></h2>`;
+        
+        movies += `<div>
+                        <h2>${movie.title}</h2>
+                        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"></img>
+                    </div>`;
 
-		let movies = " ";
-		const data = await response.json();
-		data.results.forEach(movies => {
-            movies = movies + '1111';
-			return movies;
+        // moviesVote += `<p>${movie.vote_average}</p>`;
+    });
 
-		});
-
-		document.getElementById('content').innerHTML = movies;
-
-	} catch(error) {
-
-		console.log(error);
-	}
-
-
-}
-
+    document.getElementById('movieContent').innerHTML = movies; // Display the movie titles on the webpage
+    // document.getElementById('poster').innerHTML = moviesPoster; // Display posters on the webpage
+    // document.getElementById('vote').innerHTML = moviesVote; // Display votes average
+  
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 loadMovies();
 
-
-// function getWeather(){
+// function xxxxxxx(){
 // 	fetch(API_URL)
 // 	.then(res => res.json())
 // 	.then(data => {
