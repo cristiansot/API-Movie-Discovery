e932220673c16ee9a432c6c28ef38a27eee1ae5d
cristiansot <soluciongrafix@gmail.com>
2023-11-09 21:55:25 -0300

    Traying to develop put, delet and post

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,58 @@
 const API_KEY = `96a061b7c4dfa7a6d5bcdc9f1733e583`;
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`; // Movie API
+const btnPost = document.querySelector('#btnPost');
+const inputText = document.querySelector('#inputText');
+const message = document.querySelector('#message');
 
-const loadMovies = async () => { //https://www.youtube.com/watch?v=GtIdmfBSoHw&list=PLpYu7Wkyi5ruXL-HBsw61NmqZy2yLEqXc&index=62&ab_channel=CodingInColour
-  
-    try {
+const loadMovies = async () => {
+  let movies = '';
+  try {
     const response = await fetch(API_URL);
-
-    let movies = '';
-    // let moviesPoster = '';
-    // let moviesVote = '';
     const data = await response.json();
-    data.results.forEach(movie => { //goes through the array looking for the titles and posters
-
-        // movies += `<h2>${movie.title}</h2><img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"></img>`;
-        // movies += `<h2>${movie.title}<img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"></img></h2>`;
-        
+    data.results.forEach(movie => {
+      if (response.status === 200) {
         movies += `<div>
                         <h2>${movie.title}</h2>
                         <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"></img>
                     </div>`;
-
-        // moviesVote += `<p>${movie.vote_average}</p>`;
+      }
     });
+    document.getElementById('movieContent').innerHTML = movies;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    document.getElementById('movieContent').innerHTML = movies; // Display the movie titles on the webpage
-    // document.getElementById('poster').innerHTML = moviesPoster; // Display posters on the webpage
-    // document.getElementById('vote').innerHTML = moviesVote; // Display votes average
-  
+const getData = () => {
+    const inputTextValue = FormData(inputText);
+    // const inputTextValue = document.getElementById('inputText').value;
+    return getData;
+};
+
+const postData = async () => {
+    
+    const newData = getData;
+    try {
+        const response = await fetch('http://localhost:3000/post', { 
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newData)
+        });
+        if (response.ok) {
+            // const jsonResponse = await response.json();
+            // message = jsonResponse;
+           
+            document.getElementById('message').innerHTML = JSON.stringify(newData);
+
+        }
     } catch (error) {
         console.log(error);
     }
 };
 
+btnPost.addEventListener('click', (event) => {
+    event.preventDefault();
+    postData();
+});
+
 loadMovies();
-
-// function xxxxxxx(){
-// 	fetch(API_URL)
-// 	.then(res => res.json())
-// 	.then(data => {
-// 		console.log(data)
-// 	})
-// }
-
-
-// Object.keys(data.validity_checks).map(key => {
-	// 	const value = data.validity_checks[key]
-	// 	console.log({key}, value);
-	// });
-
-
